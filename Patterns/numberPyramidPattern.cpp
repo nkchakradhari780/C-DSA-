0224f5d8b2cc78395572bce53222ba40ae7cf2c7
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2026-03-08 17:05:11 +0530

    number Pyramid Pattern

--- Patterns/numberPyramidPattern.cpp
+++ Patterns/numberPyramidPattern.cpp
@@ -2,20 +2,18 @@
 using namespace std;
 
 int main(){
-    int n;
-    int j;
+    int n,j;
     cout<<"enter number n: ";
     cin>>n;
-
     for(int i=1; i<=n; i++){
         for(j=1; j<=n-i; j++){
             cout<<"  ";
         }
-        for(int k=j; k<n; k++){
-            cout<<"* ";
+        for(int k=1; k<=i; k++){
+            cout<<k<<" ";
         }
-        for(int l=1; l<=i; l++){
-            cout<<"* ";
+        for(int l=i-1; l>=1; l--){
+            cout<<l<<" ";
         }
         cout<<endl;
     }
