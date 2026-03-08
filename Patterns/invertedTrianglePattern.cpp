b5cab29a9ffd371f9d8f4267fdcd066aaef990e8
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2026-03-08 16:23:08 +0530

    Inverted Triangle Pattern

---
+++ Patterns/invertedTrianglePattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter number n: ";
+    cin>>n;
+    for (int i=1; i<=n; i++){
+        for(int j=0; j<i-1; j++){
+           cout<<"  ";
+        }
+        for(int k=i; k<=n; k++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
