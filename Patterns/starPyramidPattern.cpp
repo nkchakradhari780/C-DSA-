a20013f83dc4576791052db283a77cb898346f71
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2026-03-08 16:45:27 +0530

    star pyramid pattern

---
+++ Patterns/starPyramidPattern.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int j;
+    cout<<"enter number n: ";
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        for(j=1; j<=n-i; j++){
+            cout<<"  ";
+        }
+        for(int k=j; k<n; k++){
+            cout<<"* ";
+        }
+        for(int l=1; l<=i; l++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
