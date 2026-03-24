dd3e8bd39e85b9a27e78cd49a2dab2204ea9e176
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2026-03-24 18:41:43 +0530

    smallest number

---
+++ smallestNumber.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int smallestNum(int arr[], int size){       // log(n) time complexity 
+    int smallest = INT_MAX;
+    for(int i = 0; i < size; i++){
+        if(arr[i] < smallest){
+            smallest = arr[i];
+        }
+    }
+    return smallest;
+}
+
+int main(){
+    int NumArr[5];
+    cout<<"Enter the 5 Numbers: ";
+    for(int i = 0; i< 5; i++){
+        cin>>NumArr[i];
+    }
+    int smallest = smallestNum(NumArr, 5);
+    cout<<"Smallest Number is: "<< smallest;
+    return 0;
+}
